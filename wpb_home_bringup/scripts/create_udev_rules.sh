4e041c15889cd626fc07d0426fd4bf8ec24cae0c
ZhangWanjie <zhangwanjie@126.com>
2017-03-25 11:20:40 +0800

    add kinect2 rules

--- wpb_home_bringup/scripts/create_udev_rules.sh
+++ wpb_home_bringup/scripts/create_udev_rules.sh
@@ -9,6 +9,10 @@ echo "remap the device serial port(ttyUSBX) to rplidar"
 echo "start copy rplidar.rules to  /etc/udev/rules.d/"
 sudo cp `rospack find wpb_home_bringup`/scripts/rplidar.rules  /etc/udev/rules.d
 
+echo "set kinect2 rules"
+echo "start copy 90-kinect2.rules to  /etc/udev/rules.d/"
+sudo cp `rospack find wpb_home_bringup`/scripts/90-kinect2.rules  /etc/udev/rules.d
+
 echo "Restarting udev"
 sudo service udev reload
 sudo service udev restart
--- wpb_home_bringup/scripts/delete_udev_rules.sh
+++ wpb_home_bringup/scripts/delete_udev_rules.sh
@@ -3,6 +3,10 @@
 echo "***************"
 echo "delete the remap device serial port to ftdi"
 sudo rm   /etc/udev/rules.d/ftdi.rules
+echo "delete the remap device serial port to rplidar"
+sudo rm   /etc/udev/rules.d/rplidar.rules
+echo "delete the kinect2 rules"
+sudo rm   /etc/udev/rules.d/90-kinect2.rules
 echo "Restarting udev"
 sudo service udev reload
 sudo service udev restart

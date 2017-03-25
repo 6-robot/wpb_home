88d89bbd317bfd7da6dcd95d8de4a51a2e883098
ZhangWanjie <zhangwanjie@126.com>
2017-03-25 11:13:56 +0800

    add rplidar udev rules

--- wpb_home_bringup/scripts/create_udev_rules.sh
+++ wpb_home_bringup/scripts/create_udev_rules.sh
@@ -4,6 +4,11 @@ echo "***************"
 echo "remap the device serial port(ttyUSBX) to ftdi"
 echo "start copy ftdi.rules to  /etc/udev/rules.d/"
 sudo cp `rospack find wpb_home_bringup`/scripts/ftdi.rules  /etc/udev/rules.d
+
+echo "remap the device serial port(ttyUSBX) to rplidar"
+echo "start copy rplidar.rules to  /etc/udev/rules.d/"
+sudo cp `rospack find wpb_home_bringup`/scripts/rplidar.rules  /etc/udev/rules.d
+
 echo "Restarting udev"
 sudo service udev reload
 sudo service udev restart

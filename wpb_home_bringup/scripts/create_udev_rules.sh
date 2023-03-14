37cdc07d15bf5b6a593df7b3e17737caefb0ff7a
zhangwanjie <zhangwanjie@126.com>
2023-03-14 22:05:48 +0800

    update

--- wpb_home_bringup/scripts/create_udev_rules.sh
+++ wpb_home_bringup/scripts/create_udev_rules.sh
@@ -4,14 +4,17 @@ echo "***************"
 echo "remap the device serial port(ttyUSBX) to ftdi"
 echo "start copy ftdi.rules to  /etc/udev/rules.d/"
 sudo cp `rospack find wpb_home_bringup`/scripts/ftdi.rules  /etc/udev/rules.d
+sudo cp ftdi.rules  /etc/udev/rules.d
 
 echo "remap the device serial port(ttyUSBX) to rplidar"
 echo "start copy rplidar.rules to  /etc/udev/rules.d/"
 sudo cp `rospack find wpb_home_bringup`/scripts/rplidar.rules  /etc/udev/rules.d
+sudo cp rplidar.rules  /etc/udev/rules.d
 
 echo "set kinect2 rules"
 echo "start copy 90-kinect2.rules to  /etc/udev/rules.d/"
 sudo cp `rospack find wpb_home_bringup`/scripts/90-kinect2.rules  /etc/udev/rules.d
+sudo cp 90-kinect2.rules  /etc/udev/rules.d
 
 echo "Restarting udev"
 sudo service udev reload
